5aca08f334be1fb7261b2c32b83c6007b986a507
szyhdev <szyuanhua@163.com>
2023-01-03 10:36:47 +0800

    add chapter 01 source code

---
+++ 01_总论/02/010204.cc
@@ -0,0 +1,19 @@
+// g++ 010204.cc -o 010204
+
+#include <iostream>
+using namespace std;
+
+#include <boost/version.hpp>
+#include <boost/config.hpp>
+using namespace boost;
+
+int main() {
+    cout << BOOST_VERSION << endl;
+    cout << BOOST_LIB_VERSION << endl;
+    cout << BOOST_PLATFORM << endl;
+    cout << BOOST_COMPILER << endl;
+    cout << BOOST_STDLIB << endl;
+
+    return 0;
+}
+
